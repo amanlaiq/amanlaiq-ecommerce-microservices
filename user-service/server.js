0138272c905f330f24f812235875b8875f2488e1
amanlaiq <amoha211@uic.edu>
2024-10-30 23:32:15 -0500

    updated server.js

--- user-service/server.js
+++ user-service/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Configure CORS properly
+app.use(cors({
+  origin: 'http://localhost:3006', // Adjust this as needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specific methods you allow
+  credentials: true, // If you need to handle cookies
+}));
+
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes); // Use the imported routes
 
 mongoose.connect('mongodb://mongo:27017/userService', {
   useNewUrlParser: true,
